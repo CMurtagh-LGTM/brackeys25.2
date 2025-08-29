b96bc2e56c934d4ae3f8b68c8c241a5b4ba4187c
cameron <cameron.murtagh00@gmail.com>
2025-08-30 03:53:34 +1000

    buff peek action

--- Triumph/TriumphAction/PeekAction.gd
+++ Triumph/TriumphAction/PeekAction.gd
@@ -1,12 +1,15 @@
 class_name PeekAction
 extends TriumphAction
 
+@export var count: int = 1
+
 func before_bid(state: TriumphGameState) -> void:
 
 	for hand: Hand in state.hands:
 		if hand == state.player:
 			continue
-		hand.random_card().reveal()
+		for _i: int in count:
+			hand.random_card().reveal()
 
 func has_before_bid(_state: TriumphGameState) -> bool:
 	return true
